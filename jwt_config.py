8f78020760cc000cda088a484c0f8c320cff6cab
Marcos Paricio <marcos.paricio@nexiona.com>
2024-09-30 13:51:54 +0200

    jwt_secret env variable for token

--- jwt_config.py
+++ jwt_config.py
@@ -1,9 +1,16 @@
 from jwt import encode, decode
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # carga las variables de entorno del archivo .env
+
+JWT_SECRET = os.getenv('FAST_API_JWT_SECRET')
+
 def dame_token(dato: dict) -> str:
-    token: str = encode(payload=dato, key='mi_clave', algorithm='HS256')
+    token: str = encode(payload=dato, key=JWT_SECRET, algorithm='HS256')
     return token
 
 def validar_token(token: str) -> dict:
-    dato: dict = decode(token, key='mi_clave', algorithms=['HS256'])
+    dato: dict = decode(token, key=JWT_SECRET, algorithms=['HS256'])
     return dato
